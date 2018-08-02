20dc596c53efb0c0e79deab8f468b94cb0fdd62c
AjitSingh <jeetsingh.ajit@gmail.com>
2018-08-02 20:39:12 +0530

    Adds functionality to convert 8 into roman numbers and refactored tests

--- src/test/java/com/thoughtworks/converter/RomanNumberConverterTest.java
+++ src/test/java/com/thoughtworks/converter/RomanNumberConverterTest.java
@@ -1,26 +1,37 @@
 package com.thoughtworks.converter;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class RomanNumberConverterTest {
+
+    private RomanNumberConverter converter;
+
+    @Before
+    public void setUp() {
+        converter = new RomanNumberConverter();
+    }
+
     @Test
     public void shouldReturnRomanNumberFor_1() {
-        RomanNumberConverter converter = new RomanNumberConverter();
         assertThat(converter.convert(1), is("I"));
     }
 
     @Test
     public void shouldReturnRomanNumberFor_3() {
-        RomanNumberConverter converter = new RomanNumberConverter();
         assertThat(converter.convert(3), is("III"));
     }
 
     @Test
     public void shouldReturnRomanNumberFor_5() {
-        RomanNumberConverter converter = new RomanNumberConverter();
         assertThat(converter.convert(5), is("V"));
+    }
+
+    @Test
+    public void shouldReturnRomanNumberFor_8() {
+        assertThat(converter.convert(8), is("VIII"));
     }
 }
